ee8054c5dc955a1935132c234e07f4fb8f62ac54
shockdude <shockdudex@gmail.com>
2021-02-14 02:50:51 -0800

    file encoding detection manual .ini parsing support T1 GEMS midi track more exception handling to prevent halting a batch conversion

--- chart-to-sm.py
+++ chart-to-sm.py
@@ -19,15 +19,15 @@
 # Built from the original chart-to-sm.js by Paturages, released under GPL3 with his permission
 
 import re
-import configparser
 import math
 import os
 import sys
 import traceback
+import codecs
 # hacked mido 1.2.9 to support sysex data bytes > 127, used for tap notes
 import mido_sysexhack as mido
 
-VERSION = "v0.2"
+VERSION = "v0.3"
 
 CHART_EXT = ".chart"
 MID_EXT = ".mid"
@@ -64,6 +64,30 @@ def measure_gcd(num_set, measure_length):
 		if d == 1:
 			return d
 	return d;
+
+# based on https://stackoverflow.com/a/65841914
+def check_encoding(infile):
+	with open(infile, "rb") as f:
+		beginning = f.read(4)
+		# The order of these if-statements is important
+		# otherwise UTF32 LE may be detected as UTF16 LE as well
+		if beginning == codecs.BOM_UTF32_LE:
+			return "utf_32_be"
+		elif beginning == codecs.BOM_UTF32_BE:
+			return "utf_32_le"
+		elif beginning[0:3] == codecs.BOM_UTF8:
+			return "utf_8_sig"
+		elif beginning[0:2] == codecs.BOM_UTF16_LE:
+			return "utf_16_le"
+		elif beginning[0:2] == codecs.BOM_UTF16_BE:
+			return "utf_16_be"
+	# check if utf-8
+	try:
+		with open(infile, "r", encoding="utf-8") as f:
+			f.read()
+		return "utf-8"
+	except:
+		return "cp1252"
 
 def output_sm(notes, last_note, measure_length, sm_diff, diff_value):
 	sm_notes = ''
@@ -104,29 +128,30 @@ def output_sm(notes, last_note, measure_length, sm_diff, diff_value):
 
 def process_song_ini(bpms):
 	# load the song.ini
-	songini = configparser.ConfigParser()
+	songini_encoding = check_encoding(SONG_INI)
+	songdata = {}
 	try:
-		songini.read(SONG_INI)
+		with open(SONG_INI, "r", encoding=songini_encoding) as songini_file:
+			for line in songini_file:
+				split_line = line.split("=", 1)
+				if len(split_line) == 2:
+					songdata[split_line[0].strip().lower()] = split_line[1].strip()
 	except:
 		traceback.print_exc()
-		print("Failed to parse chart song.ini")
-		return 1, 1
+		print("Failed to parse song.ini")
+		songdata = {}
 	
 	# look for the song audio file
 	song_file = None
 	for file in SONG_FILES:
 		if os.path.isfile(file):
-			song_file = file
-			break
+			if song_file == None:
+				song_file = file
+			else:
+				print("Warning: found song {} & stem {}. Stems currently not supported in SM".format(song_file, file))
 	if song_file == None:
 		print("Warning: Audio file not found for chart")
 		song_file = "song.ogg"
-	
-	sections = songini.sections()
-	if len(sections) != 1:
-		print("Malformed song.ini - unexpected number of sections")
-		return 1, 1
-	songdata = songini[sections[0]]
 
 	# string to contain the .sm header
 	sm_header = ''
@@ -158,12 +183,12 @@ def process_song_ini(bpms):
 		
 	return sm_header, diff_guitar
 
-def chart_get_notes(infile, diff_map, diff_value, measure_length):
+def chart_get_notes(infile, diff_map, diff_value, measure_length, infile_encoding):
 	# create a map to access notes by their index (<index> = N 0 0)
 	notes = {}
 	last_note = 0
 	ch_diff, sm_diff = diff_map # e.g. [ExpertSingle], Challenge:
-	with open(infile, "r", encoding="utf-8") as chartfile:
+	with open(infile, "r", encoding=infile_encoding) as chartfile:
 		line = "\n"
 		while len(line) > 0 and line.find(ch_diff) < 0: # e.g. [ExpertSingle]
 			line = chartfile.readline()
@@ -212,7 +237,8 @@ def chart_to_sm(infile):
 	# look for [Song] and chart resolution
 	chart_resolution = 0
 	measure_length = 0
-	with open(infile, "r", encoding="utf-8") as chartfile:
+	infile_encoding = check_encoding(infile)
+	with open(infile, "r", encoding=infile_encoding) as chartfile:
 		line = "\n"
 		while len(line) > 0 and line.find("[Song]") < 0:
 			line = chartfile.readline()
@@ -225,7 +251,7 @@ def chart_to_sm(infile):
 	
 	# look for [SyncTrack] and BPMs
 	bpms = "#BPMS:"
-	with open(infile, "r", encoding="utf-8") as chartfile:
+	with open(infile, "r", encoding=infile_encoding) as chartfile:
 		line = "\n"
 		while len(line) > 0 and line.find("[SyncTrack]") < 0:
 			line = chartfile.readline()
@@ -250,7 +276,7 @@ def chart_to_sm(infile):
 	with open("notes.sm", "w", encoding="utf-8") as outfile:
 		outfile.write(sm_header)					
 		for diffmap in DIFFMAPPINGS:
-			sm_notes = chart_get_notes(infile, diffmap, diff_guitar, measure_length)
+			sm_notes = chart_get_notes(infile, diffmap, diff_guitar, measure_length, infile_encoding)
 			if len(sm_notes) > 0:
 				outfile.write(sm_notes)
 				
@@ -328,6 +354,7 @@ def mid_to_sm(infile):
 		return 1
 	track_tempomap = None
 	track_guitar = None
+	track_t1gems = None
 	track_rhythm = None
 	track_bass = None
 	track_notes = None
@@ -342,6 +369,8 @@ def mid_to_sm(infile):
 		else:
 			if track.name == "PART GUITAR":
 				track_guitar = track
+			elif track.name == "T1 GEMS":
+				track_t1gems = track
 			elif track.name == "PART RHYTHM":
 				track_rhythm = track
 			elif track.name == "PART BASS":
@@ -351,6 +380,8 @@ def mid_to_sm(infile):
 	# so pick 1 guitar track in the midi to use
 	if track_guitar != None:
 		track_notes = track_guitar
+	elif track_t1gems != None:
+		track_notes = track_t1gems
 	elif track_rhythm != None:
 		track_notes = track_rhythm
 	elif track_bass != None:
@@ -412,10 +443,14 @@ def scan_folder(in_folder):
 			os.chdir(f)
 			scan_folder(f)
 			os.chdir("..")
-	if os.path.isfile(NOTES_NAME+MID_EXT):
-		handle_file(NOTES_NAME+MID_EXT)
-	elif os.path.isfile(NOTES_NAME+CHART_EXT):
-		handle_file(NOTES_NAME+CHART_EXT)
+	try:
+		if os.path.isfile(NOTES_NAME+MID_EXT):
+			handle_file(NOTES_NAME+MID_EXT)
+		elif os.path.isfile(NOTES_NAME+CHART_EXT):
+			handle_file(NOTES_NAME+CHART_EXT)
+	except:
+		traceback.print_exc()
+		print("Failed to process chart in {}".format(in_folder))
 
 def main():
 	# force utf-8 in stdout
